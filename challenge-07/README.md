4fee41bf4543a4d7e11c744ea591c86e944e3fa1
Nathalia Cristina Gomes Oliveira <nathaliacristina20@hotmail.com>
2020-07-06 20:26:15 -0300

    challenge 07

---
+++ challenge-07/README.md
@@ -0,0 +1,22 @@
+<h1 align="center">GoFinances</h1>
+
+<blockquote align="center">"Não espere resultados brilhantes se suas metas não forem claras!"</blockquote>
+
+## Sobre o desafio
+
+<p>Neste desafio deverá ser desenvolvida uma aplicação de gestao de transações, a GoFinances, para praticar tudo o que foi aprendido ate agora com React.js e Typescript, utilizando rotas e envio de arquivos por formulário.
+Esta aplicação irá se conectar com o backend do <a href="https://github.com/nathaliacristina20/gostack-typeorm-upload">desafio 06</a>.</p>
+
+## Funcionalidades da aplicação
+
+- Exibir uma listagem de todas as transações que estão cadastradas na sua API.
+- Exibir o balance que é retornado do seu backend, contendo o total geral, junto ao total de entradas e saídas.
+- Permitir o envio de um arquivo no formato csv para o seu backend, que irá fazer a importação das transações para o seu banco de dados.
+
+## Layout da aplicação
+
+O layout pode ser acessado através da página do Figma, no <a href="https://www.figma.com/file/EgOhyj1Inz14dhWGVhRlhr/GoFinances?node-id=1%3A863">seguinte link</a>.
+
+
+---
+console.log('Feito com :heart: por [Nathalia](https://www.linkedin.com/in/nathaliagomesoliveira/)'); 
---
+++ challenge-07/src/pages/Dashboard/index.tsx
@@ -0,0 +1,127 @@
+import React, { useState, useEffect } from 'react';
+
+import { format, parseISO } from 'date-fns';
+
+import income from '../../assets/income.svg';
+import outcome from '../../assets/outcome.svg';
+import total from '../../assets/total.svg';
+
+import api from '../../services/api';
+
+import Header from '../../components/Header';
+
+import formatValue from '../../utils/formatValue';
+
+import { Container, CardContainer, Card, TableContainer } from './styles';
+
+interface Transaction {
+  id: string;
+  title: string;
+  value: number;
+  formattedValue: string;
+  formattedDate: string;
+  type: 'income' | 'outcome';
+  category: { title: string };
+  created_at: Date;
+}
+
+interface Balance {
+  income: string;
+  outcome: string;
+  total: string;
+}
+
+const Dashboard: React.FC = () => {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance>({} as Balance);
+
+  useEffect(() => {
+    async function loadTransactions(): Promise<void> {
+      const { data } = await api.get('transactions');
+
+      const formattedTransactions = data.transactions.map(
+        (transaction: Transaction) => ({
+          ...transaction,
+          formattedValue:
+            transaction.type === 'income'
+              ? `${formatValue(transaction.value)}`
+              : `- ${formatValue(transaction.value)}`, // eslint-disable-next-line
+          created_at: format(parseISO(String(transaction.created_at)), "dd'/'MM'/'yyyy")
+        }),
+      );
+
+      const formattedBalance = {
+        income: formatValue(Number(data.balance.income)),
+        outcome: formatValue(Number(data.balance.outcome)),
+        total: formatValue(Number(data.balance.total)),
+      };
+
+      setTransactions(formattedTransactions);
+      setBalance(formattedBalance);
+    }
+
+    loadTransactions();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <CardContainer>
+          <Card>
+            <header>
+              <p>Entradas</p>
+              <img src={income} alt="Income" />
+            </header>
+            <h1 data-testid="balance-income">{balance.income}</h1>
+          </Card>
+          <Card>
+            <header>
+              <p>Saídas</p>
+              <img src={outcome} alt="Outcome" />
+            </header>
+            <h1 data-testid="balance-outcome">{balance.outcome}</h1>
+          </Card>
+          <Card total>
+            <header>
+              <p>Total</p>
+              <img src={total} alt="Total" />
+            </header>
+            <h1 data-testid="balance-total">{balance.total}</h1>
+          </Card>
+        </CardContainer>
+
+        <TableContainer>
+          <table>
+            <thead>
+              <tr>
+                <th>Título</th>
+                <th>Preço</th>
+                <th>Categoria</th>
+                <th>Data</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {transactions &&
+                transactions.map(transaction => (
+                  <React.Fragment key={transaction.id}>
+                    <tr>
+                      <td className="title">{transaction.title}</td>
+                      <td className={transaction.type}>
+                        {transaction.formattedValue}
+                      </td>
+                      <td>{transaction.category.title}</td>
+                      <td>{transaction.created_at}</td>
+                    </tr>
+                  </React.Fragment>
+                ))}
+            </tbody>
+          </table>
+        </TableContainer>
+      </Container>
+    </>
+  );
+};
+
+export default Dashboard;
